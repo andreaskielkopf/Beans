91936b6f2dd402a913fce83a01f3f424e6621b5d
Andreas Kielkopf <andreas.kielkopf@gmail.com>
2026-05-26 13:14:33 +0200

    SimplePair

---
+++ src/de/uhingen/kielkopf/andreas/beans/data/SimplePair.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package de.uhingen.kielkopf.andreas.beans.data;
+
+/**
+ * Enfaches Paar von 2 Objekten eines vorgegebenen Typs
+ * 
+ * @author Andreas Kielkopf
+ * @param <T1>
+ *           a
+ * @param <T2>
+ *           b
+ *
+ */
+public class SimplePair<T1, T2> {
+   @SuppressWarnings("null") private T1 a=null;
+   @SuppressWarnings("null") private T2 b=null;
+   /**
+    * @param a_
+    *           T1
+    * @param b_
+    *           T2
+    * 
+    */
+   public SimplePair(T1 a_, T2 b_) {
+      setA(a_);
+      setB(b_);
+   }
+   /**
+    * @return T1
+    */
+   public T1 getA() {
+      return a;
+   }
+   /**
+    * @param a_
+    *           T1
+    * 
+    */
+   public void setA(T1 a_) {
+      if (a_ instanceof T1 t1)
+         this.a=t1;
+   }
+   /**
+    * @return T2
+    */
+   public T2 getB() {
+      return b;
+   }
+   /**
+    * @param b_
+    *           T2
+    * 
+    */
+   public void setB(T2 b_) {
+      if (b_ instanceof T2 t2)
+         this.b=t2;
+   }
+}
